ec036efc30fa5f3726d23e326982bb84ff7c8e6a
productdevbook <hi@productdevbook.com>
2023-02-19 10:49:18 +0300

    chore: lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
-import animejs from 'animejs';
-import type { AnimeParams, Targets, AnimeInstance } from './type';
+import animejs from 'animejs'
+import type { AnimeInstance, AnimeParams, Targets } from './type'
 
 function arrayAnimations(_anime: (params: AnimeParams) => AnimeInstance) {
-    const fade = (value: { target: Targets, params?: AnimeParams }) => {
-        return _anime({
-            targets: value.target,
-            delay: animejs.stagger(100),
-            opacity: [0, 1],
-            ...value.params
-        });
-    }
+  const fade = (value: { target: Targets; params?: AnimeParams }) => {
+    return _anime({
+      targets: value.target,
+      delay: animejs.stagger(100),
+      opacity: [0, 1],
+      ...value.params,
+    })
+  }
 
-    return {
-        fade
-    }
+  return {
+    fade,
+  }
 }
 
-
 export function useAnimejs() {
-    const _anime = (params: AnimeParams) => {
-        return animejs(params)
-    }
+  const _anime = (params: AnimeParams) => {
+    return animejs(params)
+  }
 
-    const array = arrayAnimations(_anime);
+  const array = arrayAnimations(_anime)
 
-    return {
-        anime: _anime,
-        array
-    }
+  return {
+    anime: _anime,
+    array,
+  }
 }
--- src/type.ts
+++ src/type.ts
@@ -5,146 +5,145 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.4
 
-type FunctionBasedParameter = (element: HTMLElement, index: number, length: number) => number;
-type AnimeCallbackFunction = (anim: anime.AnimeInstance) => void;
-type CustomEasingFunction = (el: HTMLElement, index: number, length: number) => ((time: number) => number);
+type FunctionBasedParameter = (element: HTMLElement, index: number, length: number) => number
+type AnimeCallbackFunction = (anim: anime.AnimeInstance) => void
+type CustomEasingFunction = (el: HTMLElement, index: number, length: number) => ((time: number) => number)
 // Allowing null is necessary because DOM queries may not return anything.
-type AnimeTarget = string | object | HTMLElement | SVGElement | NodeList | null;
+type AnimeTarget = string | object | HTMLElement | SVGElement | NodeList | null
 
 type EasingOptions =
-    | "linear"
-    | "easeInQuad"
-    | "easeInCubic"
-    | "easeInQuart"
-    | "easeInQuint"
-    | "easeInSine"
-    | "easeInExpo"
-    | "easeInCirc"
-    | "easeInBack"
-    | "easeInElastic"
-    | "easeInBounce"
-    | "easeOutQuad"
-    | "easeOutCubic"
-    | "easeOutQuart"
-    | "easeOutQuint"
-    | "easeOutSine"
-    | "easeOutExpo"
-    | "easeOutCirc"
-    | "easeOutBack"
-    | "easeOutElastic"
-    | "easeOutBounce"
-    | "easeInOutQuad"
-    | "easeInOutCubic"
-    | "easeInOutQuart"
-    | "easeInOutQuint"
-    | "easeInOutSine"
-    | "easeInOutExpo"
-    | "easeInOutCirc"
-    | "easeInOutBack"
-    | "easeInOutElastic"
-    | "easeInOutBounce";
-type DirectionOptions = "reverse" | "alternate" | "normal";
+    | 'linear'
+    | 'easeInQuad'
+    | 'easeInCubic'
+    | 'easeInQuart'
+    | 'easeInQuint'
+    | 'easeInSine'
+    | 'easeInExpo'
+    | 'easeInCirc'
+    | 'easeInBack'
+    | 'easeInElastic'
+    | 'easeInBounce'
+    | 'easeOutQuad'
+    | 'easeOutCubic'
+    | 'easeOutQuart'
+    | 'easeOutQuint'
+    | 'easeOutSine'
+    | 'easeOutExpo'
+    | 'easeOutCirc'
+    | 'easeOutBack'
+    | 'easeOutElastic'
+    | 'easeOutBounce'
+    | 'easeInOutQuad'
+    | 'easeInOutCubic'
+    | 'easeInOutQuart'
+    | 'easeInOutQuint'
+    | 'easeInOutSine'
+    | 'easeInOutExpo'
+    | 'easeInOutCirc'
+    | 'easeInOutBack'
+    | 'easeInOutElastic'
+    | 'easeInOutBounce'
+type DirectionOptions = 'reverse' | 'alternate' | 'normal'
 
 interface AnimeCallBack {
-    begin?: AnimeCallbackFunction | undefined;
-    change?: AnimeCallbackFunction | undefined;
-    update?: AnimeCallbackFunction | undefined;
-    complete?: AnimeCallbackFunction | undefined;
-    loopBegin?: AnimeCallbackFunction | undefined;
-    loopComplete?: AnimeCallbackFunction | undefined;
-    changeBegin?: AnimeCallbackFunction | undefined;
-    changeComplete?: AnimeCallbackFunction | undefined;
+  begin?: AnimeCallbackFunction | undefined
+  change?: AnimeCallbackFunction | undefined
+  update?: AnimeCallbackFunction | undefined
+  complete?: AnimeCallbackFunction | undefined
+  loopBegin?: AnimeCallbackFunction | undefined
+  loopComplete?: AnimeCallbackFunction | undefined
+  changeBegin?: AnimeCallbackFunction | undefined
+  changeComplete?: AnimeCallbackFunction | undefined
 }
 
 export interface AnimeInstanceParams extends AnimeCallBack {
-    loop?: number | boolean | undefined;
-    autoplay?: boolean | undefined;
-    direction?: DirectionOptions | string | undefined;
+  loop?: number | boolean | undefined
+  autoplay?: boolean | undefined
+  direction?: DirectionOptions | string | undefined
 }
 
 interface AnimeAnimParams extends AnimeCallBack {
-    targets?: AnimeTarget | ReadonlyArray<AnimeTarget> | undefined;
+  targets?: AnimeTarget | ReadonlyArray<AnimeTarget> | undefined
 
-    duration?: number | FunctionBasedParameter | undefined;
-    delay?: number | FunctionBasedParameter | undefined;
-    endDelay?: number | FunctionBasedParameter | undefined;
-    elasticity?: number | FunctionBasedParameter | undefined;
-    round?: number | boolean | FunctionBasedParameter | undefined;
-    keyframes?: ReadonlyArray<AnimeAnimParams> | undefined;
+  duration?: number | FunctionBasedParameter | undefined
+  delay?: number | FunctionBasedParameter | undefined
+  endDelay?: number | FunctionBasedParameter | undefined
+  elasticity?: number | FunctionBasedParameter | undefined
+  round?: number | boolean | FunctionBasedParameter | undefined
+  keyframes?: ReadonlyArray<AnimeAnimParams> | undefined
 
-    easing?: EasingOptions | string | CustomEasingFunction | ((el: HTMLElement) => string) | undefined;
+  easing?: EasingOptions | string | CustomEasingFunction | ((el: HTMLElement) => string) | undefined
 
-    [AnyAnimatedProperty: string]: any;
+  [AnyAnimatedProperty: string]: any
 }
 
 export interface AnimeParams extends AnimeInstanceParams, AnimeAnimParams {
-    // Just need this to merge both Params interfaces.
+  // Just need this to merge both Params interfaces.
 }
 
 interface Animatable {
-    id: number;
-    target: HTMLElement;
-    total: number;
-    transforms: object;
+  id: number
+  target: HTMLElement
+  total: number
+  transforms: object
 }
 
 interface Animation {
-    animatable: Animatable;
-    currentValue: string;
-    delay: number;
-    duration: number;
-    endDelay: number;
-    property: string;
-    tweens: ReadonlyArray<object>;
-    type: string;
+  animatable: Animatable
+  currentValue: string
+  delay: number
+  duration: number
+  endDelay: number
+  property: string
+  tweens: ReadonlyArray<object>
+  type: string
 }
 
 export interface AnimeInstance extends AnimeCallBack {
-    play(): void;
-    pause(): void;
-    restart(): void;
-    reverse(): void;
-    seek(time: number): void;
-    tick(time: number): void;
+  play(): void
+  pause(): void
+  restart(): void
+  reverse(): void
+  seek(time: number): void
+  tick(time: number): void
 
-    began: boolean;
-    paused: boolean;
-    completed: boolean;
-    finished: Promise<void>;
+  began: boolean
+  paused: boolean
+  completed: boolean
+  finished: Promise<void>
 
-    autoplay: boolean;
-    currentTime: number;
-    delay: number;
-    direction: string;
-    duration: number;
-    loop: number | boolean;
-    timelineOffset: number;
-    progress: number;
-    remaining: number;
-    reversed: boolean;
+  autoplay: boolean
+  currentTime: number
+  delay: number
+  direction: string
+  duration: number
+  loop: number | boolean
+  timelineOffset: number
+  progress: number
+  remaining: number
+  reversed: boolean
 
-    animatables: ReadonlyArray<Animatable>;
-    animations: ReadonlyArray<Animation>;
+  animatables: ReadonlyArray<Animatable>
+  animations: ReadonlyArray<Animation>
 }
 
 interface AnimeTimelineAnimParams extends AnimeAnimParams {
-    timelineOffset: number | string | FunctionBasedParameter;
+  timelineOffset: number | string | FunctionBasedParameter
 }
 
 interface AnimeTimelineInstance extends AnimeInstance {
-    add(params: AnimeAnimParams, timelineOffset?: string | number): AnimeTimelineInstance;
+  add(params: AnimeAnimParams, timelineOffset?: string | number): AnimeTimelineInstance
 }
 
 interface StaggerOptions {
-    start?: number | string | undefined;
-    direction?: 'normal' | 'reverse' | undefined;
-    easing?: CustomEasingFunction | string | EasingOptions | undefined;
-    grid?: ReadonlyArray<number> | undefined;
-    axis?: 'x' | 'y' | undefined;
-    from?: 'first' | 'last' | 'center' | number | undefined;
+  start?: number | string | undefined
+  direction?: 'normal' | 'reverse' | undefined
+  easing?: CustomEasingFunction | string | EasingOptions | undefined
+  grid?: ReadonlyArray<number> | undefined
+  axis?: 'x' | 'y' | undefined
+  from?: 'first' | 'last' | 'center' | number | undefined
 }
 
+export type Targets = AnimeTarget | ReadonlyArray<AnimeTarget> | undefined
 
-export type Targets = AnimeTarget | ReadonlyArray<AnimeTarget> | undefined;
-
-export declare function anime(params: anime.AnimeParams): anime.AnimeInstance;
+export declare function anime(params: anime.AnimeParams): anime.AnimeInstance
